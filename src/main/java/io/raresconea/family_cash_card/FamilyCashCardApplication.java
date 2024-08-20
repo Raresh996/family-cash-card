0f3ee28120280f7b67a0630cd163aa5901ac0556
Rares Conea <raresconea@yahoo.com>
2024-08-20 15:15:00 +0300

    Initial commit

---
+++ src/main/java/io/raresconea/family_cash_card/FamilyCashCardApplication.java
@@ -0,0 +1,13 @@
+package io.raresconea.family_cash_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyCashCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/raresconea/family_cash_card/FamilyCashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.raresconea.family_cash_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
